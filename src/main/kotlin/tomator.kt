f9c1119260d921b1efd409c4be278010b222f56c
Jiri Bajger <lotbr101@gmail.com>
2018-02-22 21:07:16 +0100

    it downloads pictures now

--- src/main/kotlin/tomator.kt
+++ src/main/kotlin/tomator.kt
@@ -1,10 +1,29 @@
 import org.jsoup.Jsoup
 import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.httpGet
+import org.apache.commons.io.FilenameUtils
+import java.io.File
+import java.nio.file.Paths
+
 
 fun main(args: Array<String>){
-    val url = "http://marcopolo11.rajce.idnes.cz/Zimni_postrehy/#"
+    if(args.size == 0){
+        println("Usage: tomator <url>")
+        return
+    }
+    val url = args[0]
     val doc = Jsoup.connect(url).get()
     val links = doc.select(".photoThumb").eachAttr("href")
-    links.forEach({ print(it)})
+
+    links.forEach({ DownloadToDisk(it) })
 }
+
+fun DownloadToDisk(fileUrl: String){
+    val filename = FilenameUtils.getName(fileUrl)
+    val path = Paths.get(".").toAbsolutePath().normalize().toString()
+
+
+    Fuel.download(fileUrl).destination { response, url ->
+        File(path+"/"+filename)
+    }.response{req, res, result -> }
+}
+
